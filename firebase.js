d78d0b8745f387b77c1779d6474dd3a46a44beaa
in5production <107295716+in5production@users.noreply.github.com>
2026-01-02 16:49:07 +0300

    Refactor Firebase initialization and configuration

--- firebase.js
+++ firebase.js
@@ -1,26 +1,20 @@
-<script type="module">
-  // Import Firebase SDKs
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyDWgibGFkTYUjE9AG_mOpXW92OgP3ARvUk",
+  authDomain: "infivesite.firebaseapp.com",
+  projectId: "infivesite",
+  storageBucket: "infivesite.appspot.com",
+  messagingSenderId: "359834066542",
+  appId: "1:359834066542:web:722f2f3efabfd338a0507f",
+  measurementId: "G-49E3M70LFE"
+};
 
-  // --- Your Firebase Configuration ---
-  const firebaseConfig = {
-    apiKey: "AIzaSyDWgibGFkTYUjE9AG_mOpXW92OgP3ARvUk",
-    authDomain: "infivesite.firebaseapp.com",
-    projectId: "infivesite",
-    storageBucket: "infivesite.firebasestorage.app",
-    messagingSenderId: "359834066542",
-    appId: "1:359834066542:web:722f2f3efabfd338a0507f",
-    measurementId: "G-49E3M70LFE"
-  };
+// Initialize Firebase ONCE
+firebase.initializeApp(firebaseConfig);
 
-  // --- Initialize Firebase ---
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Services
+const auth = firebase.auth();
+const db = firebase.firestore();
 
-  // Global Firebase instances
-  window.auth = getAuth(app);
-  window.db = getFirestore(app);
-</script>
+// Google provider
+const googleProvider = new firebase.auth.GoogleAuthProvider();
